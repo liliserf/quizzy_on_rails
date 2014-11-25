e5a4dee1099790c47ece7a27d457180c29ea69cf
Lili Serfaty <lili.serfaty@gmail.com>
2014-11-25 17:59:39 -0600

    add verify endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   resources :questions
+  post "/questions/:id/verify" => "questions#verify"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
