8a8b5cb9659685289f23d79a3828eb85e81e2232
Lili Serfaty <lili.serfaty@gmail.com>
2014-11-25 17:55:22 -0600

    add show, very, render_not_found and find_question methods to questions  controller

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,7 +1,28 @@
 class QuestionsController < ApplicationController
 
+  before_action(:find_question, except: [:index])
+  
   def index
     render :json => Question.all
+  end
+
+  def show
+    render :json => @question
+  end
+
+  def verify
+    render :json => {correct: @question.answer == params[:answer]}
+  end
+
+  def render_not_found(error_message)
+    render :json => {message: error_message}, status: 404
+  end
+
+  def find_question
+    @question = Question.find_by(id: params[:id])
+    if !@question
+      render_not_found("Question not found")
+    end
   end
 
 end
